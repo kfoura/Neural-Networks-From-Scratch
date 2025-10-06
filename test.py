a8e91a9b0a3df0768b17e352c3309050c14bda2a
ConnorGoldsby <connorgoldsby@gmail.com>
2025-10-06 00:38:50 +0000

    test neuron

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello")
+import numpy as np
+class neuron:
+    def __init__(self, weight: np.array, bias: float):
+        self.weight = weight
+        self.bias = bias
+    
+    def activate(self, input: np.array) -> int:
+        return np.dot(self.weight, input) + self.bias
+
