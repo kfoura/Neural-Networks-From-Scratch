e1175ec98dcf46982f7b7d17434a9c54f9880067
Fawwaz <72813433+Dynamic6448@users.noreply.github.com>
2025-10-06 01:06:59 -0500

    meeting 1

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-inputs = [1,2,3]
+import numpy as np
+
+inputs = np.array([1, 2, 3])
+
+
+class Neuron:
+    def __init__(self, weights: np.ndarray, bias: float):
+        self.weights = weights
+        self.bias = bias
+
+    def dot(self, inputs: np.ndarray) -> float:
+        return float(np.dot(inputs, self.weights) + self.bias)
